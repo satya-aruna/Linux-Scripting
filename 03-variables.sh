8dc92a3214ce357f6919991a0a3035d8170aa888
Satya Aruna <satya.aruna@gmail.com>
2026-01-29 16:00:06 +0530

    files changed

---
+++ 03-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PERSON1="Ramesh"  # usually variable names use all caps
+PERSON2="Suresh"  # no space before and after =
+
+# variables are referred with $ sign
+
+echo "$PERSON1: Hello $PERSON2, How are you?"
+echo "$PERSON2: Hi $PERSON1, I am fine. How about you?"
+echo "$PERSON1: doing fine. What are you learning $PERSON2?"
+echo "$PERSON2: I am learning shell. What about you?"
