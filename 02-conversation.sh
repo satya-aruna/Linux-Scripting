862859b485f880d39a3741f38af9ba619165b43d
Satya Aruna <satya.aruna@gmail.com>
2026-01-29 15:50:07 +0530

    files changed

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh: Hello Suresh, How are you?"
+echo "Suresh: Hi Ramesh, I am fine. How about you?"
+echo "Ramesh: doing fine. What are you learning Suresh?"
+echo "Suresh: I am learning shell. What about you?"
