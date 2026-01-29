86eff842ab9b4a198f651cb0a72515433c9f5330
Satya Aruna <satya.aruna@gmail.com>
2026-01-29 16:46:22 +0530

    files changed

--- 07-scriptexectime.sh
+++ 07-scriptexectime.sh
@@ -7,6 +7,6 @@ echo "Script started at: $START_TIME"
 sleep 10
 
 END_TIME=$(date +%s)
-TOTAL_TIME=(($END_TIME-$START_TIME))
+TOTAL_TIME=$(($END_TIME-$START_TIME))
 
 echo "Script executed in $TOTAL_TIME seconds"
