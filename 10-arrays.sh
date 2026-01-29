48db2a266003d504810173f6a37c3a8574627c93
Satya Aruna <satya.aruna@gmail.com>
2026-01-29 19:28:42 +0530

    files changed

--- 10-arrays.sh
+++ 10-arrays.sh
@@ -7,7 +7,7 @@ FRUITS=(Apple Banana Pear Guava)
 echo "Fruits are: ${FRUITS[@]}"
 echo "Fruits count: ${#FRUITS[@]}"
 echo "First fruit: ${FRUITS[0]}"
-echo "Last fruit: ${FRUITS[((${#FRUITS[@]}-1))]}
+echo "Last fruit: ${FRUITS[((${#FRUITS[@]}-1))]}"
 
 ID=(101 102 103 104 105)
 echo "IDs are:  ${ID[@]} "
