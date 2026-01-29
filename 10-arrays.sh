56efbd013516591c2e8445f31825f1ff5913717c
Satya Aruna <satya.aruna@gmail.com>
2026-01-29 19:10:16 +0530

    files changed

--- 10-arrays.sh
+++ 10-arrays.sh
@@ -3,9 +3,8 @@
 # Array is a list of items
 
 FRUITS=(Apple Banana Pear Guava)
-echo "Fruits are: $FRUITS"
+
 echo "Fruits are: ${FRUITS[@]}
 
-ID=("101" "102" "103" "104" "105")
-echo "IDs are: $(ID)"
-echo "IDs are: ${(ID[@])}
+ID=(101 102 103 104 105)
+echo "IDs are: $(( ${ID[@]} ))"
