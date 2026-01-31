7fa3ad40c906bc899ccd46739f9ec4b601420c85
Satya Aruna <satya.aruna@gmail.com>
2026-01-31 13:25:14 +0530

    files changed

--- 15-logs.sh
+++ 15-logs.sh
@@ -5,7 +5,7 @@ LOGS_FOLDER="/var/log/linux-script"
 LOGS_FILE="$LOGS_FOLDER/$0.log"
 
 if [ $USERID -ne 0 ]; then
-    echo "Please run this script with root user access"
+    echo "Please run this script with root user access" | tee -a $LOGS_FILE
     exit 1 # we need to exit with failure exit code
 fi
 
@@ -15,10 +15,10 @@ mkdir -p $LOGS_FOLDER
 VALIDATE()
 {
     if [ $1 -ne 0 ]; then
-        echo "$2 ... FAILURE"
+        echo "$2 ... FAILURE" | tee -a $LOGS_FILE
         exit 1
     else
-        echo "$2 ... SUCCESS"
+        echo "$2 ... SUCCESS" | tee -a $LOGS_FILE
     fi
 }
 
