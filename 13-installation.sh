b9df94b3578ac02115f4b226313df893b96a1722
Satya Aruna <satya.aruna@gmail.com>
2026-01-31 12:26:51 +0530

    files changed

--- 13-installation.sh
+++ 13-installation.sh
@@ -19,3 +19,29 @@ if [ $? -ne 0 ]; then
 else
     echo "Installing Nginx ... SUCCESS"
 fi
+
+echo "Installing mysql"
+
+dnf install mysql -y
+
+# check if installation is success or failure  with $?
+
+if [ $? -ne 0 ]; then
+    echo "Installing Nginx ... FAILURE"
+    exit 1
+else
+    echo "Installing Nginx ... SUCCESS"
+fi
+
+echo "Installing nodejs"
+
+dnf install nginx -y
+
+# check if installation is success or failure  with $?
+
+if [ $? -ne 0 ]; then
+    echo "Installing Nginx ... FAILURE"
+    exit 1
+else
+    echo "Installing Nginx ... SUCCESS"
+fi
