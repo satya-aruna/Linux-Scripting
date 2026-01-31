db020f1287fad173e6d63261c754398038cb222f
Satya Aruna <satya.aruna@gmail.com>
2026-01-31 17:34:57 +0530

    files changed

--- 22-traperror.sh
+++ 22-traperror.sh
@@ -4,3 +4,10 @@ trap 'echo "An actual error occurred!"' ERR
 
 # Avoid triggering the trap if no package matches
 dnf list --installed $1 2>/dev/null || echo "Package not found, but script continues."
+echo "Installing $1 ..."
+dnf install $1 -y
+if [ $? -eq 0 ]; then
+    echo "$1 installed SUCCESSFULLY!!!"
+else
+    echo "$1 installation FAILED!!!"
+fi
