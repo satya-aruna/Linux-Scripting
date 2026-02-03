e9ce60794751423f515e5ff59d3dd2fb014d6f26
Satya Aruna <satya.aruna@gmail.com>
2026-02-03 16:30:56 +0530

    files changed

--- 26-scriptpath.sh
+++ 26-scriptpath.sh
@@ -2,4 +2,8 @@
 
 SCRIPT_DIR=$(dirname -- "$0")
 
+SCRIPT_DIR1=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 echo "Script directory name: $SCRIPT_DIR"
+
+echo "Script directory name: $SCRIPT_DIR1"
