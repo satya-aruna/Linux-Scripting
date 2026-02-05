b5cabf51e00637680576b4a839669570875c0a74
Satya Aruna <satya.aruna@gmail.com>
2026-02-05 18:42:15 +0530

    files changed

---
+++ 29-backup.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# color codes in Linux, can be enabled with echo -e option
+R='\e[31m'
+G='\e[32m'
+Y='\e[33m'
+B='\e[34m'
+N='\e[0m'
+
+
+USERID=$(id -u) # userid of root user 0, and others non-zero
+LOGS_FOLDER="/var/log/linux-script"
+LOGS_FILE="$LOGS_FOLDER/backup.log"
+SOURCE_DIR=$1
+DEST_DIR=$2
+DAYS=${3:-14} # syntax for taking $3 as days and 14 as default value if not provided
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R Please run this script with root user access $N"
+    exit 1 # we need to exit with failure exit code
+fi
+
+mkdir -p $LOGS_FOLDER
+
+USAGE() {
+    echo -e "$R sudo backup <SOURCE_DIR> <DEST_DIR> <DAYS>[default 14 days] $N"
+    exit 1
+}
+
+if [ $# -lt 2 ]; then
+    USAGE
+fi
+
+if [ ! -d SOURCE_DIR ]; then
+    echo -e "$R Source directory does not exist $N"
+    exit 1
+fi 
+
+if [ ! -d DEST_DIR ]; then
+    echo -e "$R Destination directory does not exist $N"
+    exit 1
+fi 
