2ac6704b60dacdcb2120f06abab65b3871434654
Satya Aruna <satya.aruna@gmail.com>
2026-02-08 16:53:16 +0530

    files changed

--- mail.sh
+++ mail.sh
@@ -4,7 +4,7 @@ MESSAGE_BODY=$3
 ALERT_TYPE=$4
 SERVER_IP=$5
 TO_TEAM=$6
-
+FINAL_MESSAGE_BODY=$(echo $MESSAGE_BODY | sed -e "s/'/'\\\\''/g")
 FINAL_MESSAGE=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/SERVER_IP/$SERVER_IP/g" -e "s/MESSAGE/$MESSAGE_BODY/g" template.html)
 
 {
