f0b51e36704e2051e34cc9f64aee3af9ee5feace
Satya Aruna <satya.aruna@gmail.com>
2026-02-08 17:52:24 +0530

    files changed

--- 30-diskusage.sh
+++ 30-diskusage.sh
@@ -21,7 +21,7 @@ do
     USAGE=$(echo $line | awk '{print $6}'|cut -d "%"  -f1)
     PARTITION=$(echo $line | awk '{print $NF}')
     if [ $USAGE -ge $USAGE_THRESHOLD ]; then
-        MESSAGE+="High disk usage on $PARTITION:$USAGE% <br>"
+        MESSAGE+="High disk usage on $PARTITION: $USAGE% \n"
     fi
 done <<< $DISK_USAGE
 
